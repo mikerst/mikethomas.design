f480dc41a2e8f6f7ff3c56d00c2db05348e6b666
mikerst <mikerst@gmail.com>
2019-07-22 14:24:45 +0100

    Some <head> updates including adding data to meta

--- src/_includes/js/core.js
+++ src/_includes/js/core.js
@@ -1,3 +1,6 @@
+// Some console info for curious onlookers
+console.info("Hand-made by Mike");
+
 // simple button click event handler
 function btnHandler(selector, callback) {
   var btn = document.querySelector(selector);

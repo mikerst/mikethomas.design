92c6d082641a968777dc497fed420bdf3cc9b7ec
Mike Thomas <mikerst@gmail.com>
2024-11-11 12:01:45 +0000

    Using other var

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,5 @@
 module.exports = {
   "rootUrl" : "https://mikethomas.design",
   "siteName": "Mike Thomas",
-  "buildTime" : new Date(),
-  environment: process.env.ELEVENTY_ENV || "dev"
+  "buildTime" : new Date()
 }
